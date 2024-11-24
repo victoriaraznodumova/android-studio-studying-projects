f67a4a65e77b249c5bf5c8c1d545914f41d90757
Виктория Разнодумова <raznodumova@gmail.com>
2024-11-24 03:02:19 +0300

    dates updated

--- homework2/app/src/main/java/com/misis/homework2/views/activities/MainActivity.kt
+++ homework2/app/src/main/java/com/misis/homework2/views/activities/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.misis.homework2.views.fragments.AddTransactionFragment
 import com.misis.homework2.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var calendar: Calendar = Calendar.getInstance()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +24,19 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolBar)
         supportActionBar?.title = "Семейный бюджет"
 
+
+        calendar = Calendar.getInstance();
+        updateDate();
+
         binding.floatingActionButton.setOnClickListener {
             AddTransactionFragment()
                 .show(supportFragmentManager, null)
         }
     }
+
+    fun updateDate() {
+        val dateFormat = SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault())
+        binding.currentDate.text = dateFormat.format(calendar.time)
+    }
+
 }
