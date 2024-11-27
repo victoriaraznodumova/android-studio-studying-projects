428b6b4f549cc473e5d3dbaeffb0f44bc0ca3835
Виктория Разнодумова <raznodumova@gmail.com>
2024-11-27 05:41:29 +0300

    renaming of categories

--- homework2/app/src/main/java/com/misis/homework2/utils/Constants.java
+++ homework2/app/src/main/java/com/misis/homework2/utils/Constants.java
@@ -22,12 +22,20 @@ public class Constants {
 
     public static void setCategories() {
         categories = new ArrayList<>();
-        categories.add(new Category("Salary", R.drawable.statistics, R.color.category1));
-        categories.add(new Category("Business", R.drawable.statistics, R.color.category2));
-        categories.add(new Category("Investment", R.drawable.statistics, R.color.category3));
-        categories.add(new Category("Loan", R.drawable.statistics, R.color.category4));
-        categories.add(new Category("Rent", R.drawable.statistics, R.color.category5));
-        categories.add(new Category("Other", R.drawable.statistics, R.color.category6));
+        categories.add(new Category("Заработная плата", R.drawable.ic_salary, R.color.category1));
+        categories.add(new Category("Премия", R.drawable.prize, R.color.category2));
+        categories.add(new Category("Подработка", R.drawable.ic_business, R.color.category3));
+        categories.add(new Category("Донат", R.drawable.heart, R.color.category4));
+        categories.add(new Category("Кредит", R.drawable.ic_loan, R.color.category5));
+        categories.add(new Category("Прочее", R.drawable.ic_other, R.color.category6));
+        categories.add(new Category("Продукты", R.drawable.fast_food, R.color.category7));
+        categories.add(new Category("Транспорт", R.drawable.limousine, R.color.category8));
+        categories.add(new Category("Онлайн-сервисы", R.drawable.setting, R.color.category9));
+        categories.add(new Category("Кредиты", R.drawable.credit, R.color.category10));
+        categories.add(new Category("Обучение", R.drawable.graduation, R.color.category11));
+        categories.add(new Category("Ремонт", R.drawable.reparatur, R.color.category12));
+        categories.add(new Category("Отпуск", R.drawable.sun_umbrella, R.color.category13));
+        categories.add(new Category("Хобби, развлечения", R.drawable.unterhaltungen, R.color.category14));
     }
 
     public static Category getCategoryDetails(String categoryName){
